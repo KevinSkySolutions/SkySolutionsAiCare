05f66767bf6ceb123a6d04f181dcfff540895b81
Srinivas Patnaik <srinivas@skysolutions.us>
2018-04-21 02:43:24 +0530

    Rectified the AlertItem state-ful component.

--- src/components/Dashboard/_AlertsList.subComponent.js
+++ src/components/Dashboard/_AlertsList.subComponent.js
@@ -14,9 +14,6 @@ export class AlertsList extends Component {
 
     componentWillReceiveProps(newProps) {  // Updating the state on receiving the new props after selecting a different floor
         
-        console.log("AlertsList inside componentWillReceiveProps");
-        console.log(newProps);
-
         this.setState({
             alerts: newProps.alerts
         });
@@ -26,7 +23,7 @@ export class AlertsList extends Component {
         return this.state.alerts.map((alert, keyValue) => {  // Mapping all the relevant floor alerts on the right section of the page
             return (
                 <div key={keyValue}>
-                    <AlertItem2 alert={alert} />
+                    <AlertItem alert={alert} isExpanded={true}/>
                 </div>
             )
         })
@@ -40,128 +37,87 @@ const mapStateToProps = (state) => {
 };
 export default connect(mapStateToProps)(AlertsList);
 
-function AlertItem2(props) {
+//Component for isolating each element of the Alerts List
+class AlertItem extends Component {
 
-    // TODO, conditional styling boxStyle based on reducer
-    let boxStyle = 'description-mod';  // Variable for deciding the style of the expanded or collapsed Alert
-    let divstyle = ("type-of-alert alert-number" + props.alert.priority); // Variable to decide which style to assign the alert based on the priority of the alert being passed
+    constructor(props) {
+        super(props);
 
-    let onClick = () => {
-        console.log("clicked");
-    };
+        console.log("Constructing AlertItem with below: ");
+        console.log(props.alert);
 
-    return (
-        <div className={boxStyle} onClick={onClick}>
-            <div className={divstyle}>{props.alert.type}</div>
-            <div className="alert-content-section">
-                <div className="alert-content">
-                    <div className="pt-log pt-detail">
-                        <img src={require("../../img/cardalert" + props.alert.priority + ".png")} className="avatar" />
-                        <div className="side-text detail-1 side-text-padding">
-                            <div className="pt-name list-header">{props.alert.resident}</div>
-                            <div className="pt-suite-no gray-text list-subheader mr-t-5">{props.alert.currentlocation}</div>
-                        </div>
-                    </div>
+        this.state = {
+            alert: props.alert,  // Making a copy of the alerts object for the entire facility for data manipulation
+            isExpanded: props.isExpanded,
+            style: "description-mod", // Variable for Setting the style onClick so that it expands and collapses
+            isClicked: false          // Variable for keeping tracking whether an item has been clicked or not
+        }
+    }
 
-                    <div className="pt-log pt-stat pt-stat-text">
-                        <div>
-                            <div className="help-stat list-header">
-                                {props.alert.description}
+    componentWillReceiveProps(newProps) {
+        this.setState({
+            alert:      newProps.alert,
+            isExpanded: newProps.isExpanded
+        });
+    }
+
+    onClick = e => {  // Function for changing the state and expanding or collapsing the Alert
+        if (this.props.isExpanded === false) {  // If the Alert is expanded then contract
+            this.setState({
+                style: "description-mod",
+                isClicked: true
+            });
+        }
+
+        else { // If the Alert is collapsed then expand
+            this.setState({
+                style: "description-mod-active",
+                isClicked: false
+            });
+        }
+    }
+
+    render() {
+
+        let boxStyle = this.state.style;  // Variable for deciding the style of the expanded or collapsed Alert
+        let divstyle = ("type-of-alert alert-number" + this.state.alert.priority); // Variable to decide which style to assign the alert based on the priority of the alert being passed
+
+        return (
+            <div className={boxStyle} onClick={this.onClick}>
+                <div className={divstyle}>{this.state.alert.type}</div>
+                <div className="alert-content-section">
+                    <div className="alert-content">
+                        <div className="pt-log pt-detail">
+                            <img src={require("../../img/cardalert" + this.state.alert.priority + ".png")} className="avatar" />
+                            <div className="side-text detail-1 side-text-padding">
+                                <div className="pt-name list-header">{this.state.alert.resident}</div>
+                                <div className="pt-suite-no gray-text list-subheader mr-t-5">{this.state.alert.currentlocation}</div>
                             </div>
-                            <div className="elapsed-time gray-text side-text list-subheader mr-t-5">{props.alert.time} min ago</div>
+                        </div>
+
+                        <div className="pt-log pt-stat pt-stat-text">
+                            <div>
+                                <div className="help-stat list-header">
+                                    {this.state.alert.description}
+                                </div>
+                                <div className="elapsed-time gray-text side-text list-subheader mr-t-5">{this.state.alert.time} min ago</div>
+                            </div>
                         </div>
                     </div>
+                    <div className="alert-media">
+                        {
+                            (this.state.alert.media.video === "alert.mp4")   // Conditional logic for selecting whether the alert has attached video or audio
+                                ? <MediaControl type={this.state.alert.priority} media="video" />
+                                : <MediaControl type={this.state.alert.priority} media="audio" />
+                        }
+                    </div>
                 </div>
-                <div className="alert-media">
-                    {
-                        (props.alert.media.video === "alert.mp4")   // Conditional logic for selecting whether the alert has attached video or audio
-                            ? <MediaControl type={props.alert.priority} media="video" />
-                            : <MediaControl type={props.alert.priority} media="audio" />
-                    }
-                </div>
+
+                <AlertHistory alerts={this.state.alert.history} />
             </div>
-
-            <AlertHistory alerts={props.alert.history} />
-        </div>
-    )
+        )
+    }
 }
-
-// Component for isolating each element of the Alerts List
-// class AlertItem extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             alert: this.props.alert,  // Making a copy of the alerts object for the entire facility for data manipulation
-//             style: "description-mod", // Variable for Setting the style onClick so that it expands and collapses
-//             isClicked: false          // Variable for keeping tracking whether an item has been clicked or not
-//         }
-//     }
-
-//     componentWillReceiveProps() {  // Updating the state on receiving the new props after selecting a different floor
-//         this.setState({
-//             alert: this.props.alert
-//         });
-//     }
-
-//     onClick = e => {  // Function for changing the state and expanding or collapsing the Alert
-//         if (this.state.isClicked === false) {  // If the Alert is expanded then contract
-//             this.setState({
-//                 style: "description-mod",
-//                 isClicked: true
-//             });
-//         }
-
-//         else if (this.state.isClicked === true) { // If the Alert is collapsed then expand
-//             this.setState({
-//                 style: "description-mod-active",
-//                 isClicked: false
-//             });
-//         }
-//     }
-
-//     render() {
-
-//         let boxStyle = this.state.style;  // Variable for deciding the style of the expanded or collapsed Alert
-
-//         let divstyle = ("type-of-alert alert-number" + this.state.alert.priority); // Variable to decide which style to assign the alert based on the priority of the alert being passed
-
-//         return (
-//             <div className={boxStyle} onClick={this.onClick}>
-//                 <div className={divstyle}>{this.state.alert.type}</div>
-//                 <div className="alert-content-section">
-//                     <div className="alert-content">
-//                         <div className="pt-log pt-detail">
-//                             <img src={require("../../img/cardalert" + this.state.alert.priority + ".png")} className="avatar" />
-//                             <div className="side-text detail-1 side-text-padding">
-//                                 <div className="pt-name list-header">{this.state.alert.resident}</div>
-//                                 <div className="pt-suite-no gray-text list-subheader mr-t-5">{this.state.alert.currentlocation}</div>
-//                             </div>
-//                         </div>
-
-//                         <div className="pt-log pt-stat pt-stat-text">
-//                             <div>
-//                                 <div className="help-stat list-header">
-//                                     {this.state.alert.description}
-//                                 </div>
-//                                 <div className="elapsed-time gray-text side-text list-subheader mr-t-5">{this.state.alert.time} min ago</div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className="alert-media">
-//                         {
-//                             (this.state.alert.media.video === "alert.mp4")   // Conditional logic for selecting whether the alert has attached video or audio
-//                                 ? <MediaControl type={this.state.alert.priority} media="video" />
-//                                 : <MediaControl type={this.state.alert.priority} media="audio" />
-//                         }
-//                     </div>
-//                 </div>
-
-//                 <AlertHistory alerts={this.state.alert.history} />
-//             </div>
-//         )
-//     }
-// }
 
 
 // Function for adding the history of alerts of a patient to the AlertsList Section

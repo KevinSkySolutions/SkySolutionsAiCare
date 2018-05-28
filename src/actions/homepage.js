383ca5a142a15b58b8916973560c16cd027eaec2
Srinivas Patnaik <srinivas@skysolutions.us>
2018-05-28 14:10:37 +0530

    Bug fixes.

--- src/actions/homepage.js
+++ src/actions/homepage.js
@@ -1,39 +1,132 @@
-import { REQUEST_LOGIN, REQUEST_USER_DATA, REQUST_LOGIN_FAILED, RECEIVE_USER_DATA } from '../constants';
+import { REQUEST_LOGIN, 
+    REQUEST_USER_DATA, 
+    REQUST_LOGIN_FAILED, 
+    RECEIVE_USER_DATA,
+    REQUEST_FLOOR_API_DATA,
+REQUEST_ENTERPRISE_DATA,
+REQUEST_VENUE_DATA,
+REQUEST_SENSOR_ALERT_DATA,
+REQUEST_BUILDING_DATA,
+    RECEIVE_FLOOR_API_DATA,
+RECEIVE_ENTERPRISE_DATA,
+RECEIVE_VENUE_DATA,
+RECEIVE_BUILDING_DATA,
+RECEIVE_SENSOR_ALERT_DATA } from '../constants';
 
 export const homepageActions = {
-    login,
-    requestUserData,
-    receiveUserData,
-    loginFailed
+login,
+requestUserData,
+receiveUserData,
+loginFailed,
+requestEnterpriseData,
+requestVenueData,
+requestFloorAPIData,
+requestBuildingData,
+requestSensorAlertData,
+receiveEnterpriseData,
+receiveVenueData,
+receiveFloorAPIData,
+receiveBuildingData,
+receiveSensorAlertData
 };
 
 /**
- * Action to be raised when user requests login
- * @param {string} username username of the user
- * @param {string} password password provided by the user
- */
+* Action to be raised when user requests login
+* @param {string} username username of the user
+* @param {string} password password provided by the user
+*/
 function login(username, password) {
-    // TODO, pass the username and password fields to the backend
-    return { type: REQUEST_LOGIN, payload: { } };
+// TODO, pass the username and password fields to the backend
+return { type: REQUEST_LOGIN, payload: { } };1
 }
 
 /**
- * Action to be raised when login request successful to fetch user specific data and defaults
- */
+* Action to be raised when login request successful to fetch user specific data and defaults
+*/
 function requestUserData() {
-    return { type: REQUEST_USER_DATA, payload: {} };
+return { type: REQUEST_USER_DATA, payload: {} };
 }
 
 /**
- * Action to be raised when login request successful to fetch user specific data and defaults
- */
+* Action to be raised when login request successful to fetch user specific data and defaults
+*/
 function receiveUserData(userdata) {
-    return { type: RECEIVE_USER_DATA, payload: userdata };
+return { type: RECEIVE_USER_DATA, payload: userdata };
 }
 
 /**
- * Action to be raised when login request unsuccessful
- */
- function loginFailed() {
-     return { type: REQUST_LOGIN_FAILED, payload: {} };
- }
+* Action to be raised when login request unsuccessful
+*/
+function loginFailed() {
+return { type: REQUST_LOGIN_FAILED, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function requestEnterpriseData() {
+return { type: REQUEST_ENTERPRISE_DATA, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function requestVenueData() {
+return { type: REQUEST_VENUE_DATA, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function requestFloorAPIData() {
+return { type: REQUEST_FLOOR_API_DATA, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function requestBuildingData() {
+return { type: REQUEST_BUILDING_DATA, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function requestSensorAlertData() {
+return { type: REQUEST_SENSOR_ALERT_DATA, payload: {} };
+}
+
+/**
+* Action to be raised when login request successful to fetch enterprise specific data and defaults
+*/
+function receiveEnterpriseData(enterprisedata) {
+return { type: RECEIVE_ENTERPRISE_DATA, payload: enterprisedata };
+}
+
+/**
+* Action to be raised when login receive successful to fetch enterprise specific data and defaults
+*/
+function receiveVenueData(venuedata) {
+return { type: RECEIVE_VENUE_DATA, payload: venuedata };
+}
+
+/**
+* Action to be raised when login receive successful to fetch enterprise specific data and defaults
+*/
+function receiveFloorAPIData(floorAPIdata) {
+return { type: RECEIVE_FLOOR_API_DATA, payload: floorAPIdata };
+}
+
+/**
+* Action to be raised when login receive successful to fetch enterprise specific data and defaults
+*/
+function receiveBuildingData(buildingdata) {
+return { type: RECEIVE_BUILDING_DATA, payload: buildingdata };
+}
+
+/**
+* Action to be raised when login receive successful to fetch enterprise specific data and defaults
+*/
+function receiveSensorAlertData(sensoralertdata) {
+return { type: RECEIVE_SENSOR_ALERT_DATA, payload: sensoralertdata };
+}
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,132 +1,137 @@
-import { REQUEST_LOGIN, 
-  REQUEST_USER_DATA, 
-  REQUST_LOGIN_FAILED, 
-  RECEIVE_USER_DATA,
-  REQUEST_FLOOR_API_DATA,
-  REQUEST_ENTERPRISE_DATA,
-  REQUEST_VENUE_DATA,
-  REQUEST_SENSOR_ALERT_DATA,
-  REQUEST_BUILDING_DATA,
-  RECEIVE_FLOOR_API_DATA,
-  RECEIVE_ENTERPRISE_DATA,
-  RECEIVE_VENUE_DATA,
-  RECEIVE_BUILDING_DATA,
-  RECEIVE_SENSOR_ALERT_DATA } from '../../constants';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Meta from 'react-helmet';
+import { browserHistory } from 'react-router'
 
-export const homepageActions = {
-login,
-requestUserData,
-receiveUserData,
-loginFailed,
-requestEnterpriseData,
-requestVenueData,
-requestFloorAPIData,
-requestBuildingData,
-requestSensorAlertData,
-receiveEnterpriseData,
-receiveVenueData,
-receiveFloorAPIData,
-receiveBuildingData,
-receiveSensorAlertData
-};
+import { bindActionCreators } from 'redux';
 
-/**
-* Action to be raised when user requests login
-* @param {string} username username of the user
-* @param {string} password password provided by the user
-*/
-function login(username, password) {
-// TODO, pass the username and password fields to the backend
-return { type: REQUEST_LOGIN, payload: { } };1
+import { createaction_doLogin, REQUEST_LOGIN } from '../../actions';
+
+// import (  ) from '../../reducers';
+
+import Posts from '../Patients/Patients';
+import Header from '../Common/Header/Header';
+
+// Import can't be in conditional so use require.
+if (process.env.WEBPACK) {
+  require('./HomePage.css'); // eslint-disable-line global-require
 }
 
-/**
-* Action to be raised when login request successful to fetch user specific data and defaults
+/*
+  This is the main login/landing page. 
 */
-function requestUserData() {
-return { type: REQUEST_USER_DATA, payload: {} };
+export class HomePage extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  // typechecking on the props for this component
+  static propTypes = {
+    isLoggingIn: PropTypes.bool.isRequired,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  // defaultProps, props to load before first API call is made
+  static defaultProps = {
+    isLoggingIn: false
+  }
+
+  // metadata for html
+  static getMeta() {
+    return {
+      title: 'AiCare Monitoring Portal',
+      link: [
+        {
+          rel: 'canonical',
+          href: 'http://localhost:3000'
+        }
+      ],
+      meta: [
+        {
+          charset: 'utf-8'
+        },
+        {
+          name: 'description', 
+          content: 'This is the monitoring portal for Ashby Ponds by AiCare'
+        }
+      ]
+    };
+  }
+
+  // lifecycle method
+  componentDidMount() {
+    const { dispatch } = this.props;
+  }
+
+  // the render method
+  render() {    
+
+    // for use inside render
+    const head = HomePage.getMeta();
+
+    return (
+      <div className="bodyclass">
+      <div className="content">
+        <div className="ai-care-watermark-logo" >
+             
+        </div>
+        <div className="login-credentials-field-box">
+            <img className="bg-container" src={require("../../img/logo.png")} />
+            <form className ="logindetails">
+                <div className ="information" >
+                    <img className ="login-field-icon" src={require("../../img/email-icon.png")} />
+                    <input className ="login-input-field" type="text" placeholder="Email ID" />
+                </div>
+                <div className ="information">
+                    <img className ="login-field-icon" src={require("../../img/password-icon.png")} />
+                    <input className ="login-input-field" type="password" placeholder="Password" />
+                </div>
+                <div className ="forgot">
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <div className ="signin">
+                    <input type="button" value="Sign In" onClick = { this.doLogin } />
+                </div>
+            </form>
+        </div>
+    </div>
+    </div>
+    );
+  }
+
+  doLogin = (event) => {
+
+    // TODO: remove line below and redirect on state change in Redux
+    browserHistory.push('/dashboard');
+
+    // this.props.dispatch_createaction_doLogin("u", "p");
+    this.props.dispatch({ type: REQUEST_LOGIN, payload: {username: "username", passcode: "passcode"} });
+  }
+
 }
 
-/**
-* Action to be raised when login request successful to fetch user specific data and defaults
-*/
-function receiveUserData(userdata) {
-return { type: RECEIVE_USER_DATA, payload: userdata };
+// changes in state are copied onto props here
+const mapStateToProps = (state) => {
+  
+  // loading default props
+  const { isLoggingIn } = state;
+
+  return {
+    isLoggingIn
+  };
 }
 
-/**
-* Action to be raised when login request unsuccessful
-*/
-function loginFailed() {
-return { type: REQUST_LOGIN_FAILED, payload: {} };
+// adding callables to props
+const mapDispatchToProps = (dispatch, props) => {
+
+  return Object.assign({dispatch: dispatch}, bindActionCreators({
+    dispatch_createaction_doLogin:        
+      (userid, passcode) => {
+        dispatch(createaction_doLogin(userid, passcode));
+      }
+  }, dispatch));
 }
 
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function requestEnterpriseData() {
-return { type: REQUEST_ENTERPRISE_DATA, payload: {} };
-}
-
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function requestVenueData() {
-return { type: REQUEST_VENUE_DATA, payload: {} };
-}
-
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function requestFloorAPIData() {
-return { type: REQUEST_FLOOR_API_DATA, payload: {} };
-}
-
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function requestBuildingData() {
-return { type: REQUEST_BUILDING_DATA, payload: {} };
-}
-
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function requestSensorAlertData() {
-return { type: REQUEST_SENSOR_ALERT_DATA, payload: {} };
-}
-
-/**
-* Action to be raised when login request successful to fetch enterprise specific data and defaults
-*/
-function receiveEnterpriseData(enterprisedata) {
-return { type: RECEIVE_ENTERPRISE_DATA, payload: enterprisedata };
-}
-
-/**
-* Action to be raised when login receive successful to fetch enterprise specific data and defaults
-*/
-function receiveVenueData(venuedata) {
-return { type: RECEIVE_VENUE_DATA, payload: venuedata };
-}
-
-/**
-* Action to be raised when login receive successful to fetch enterprise specific data and defaults
-*/
-function receiveFloorAPIData(floorAPIdata) {
-return { type: RECEIVE_FLOOR_API_DATA, payload: floorAPIdata };
-}
-
-/**
-* Action to be raised when login receive successful to fetch enterprise specific data and defaults
-*/
-function receiveBuildingData(buildingdata) {
-return { type: RECEIVE_BUILDING_DATA, payload: buildingdata };
-}
-
-/**
-* Action to be raised when login receive successful to fetch enterprise specific data and defaults
-*/
-function receiveSensorAlertData(sensoralertdata) {
-return { type: RECEIVE_SENSOR_ALERT_DATA, payload: sensoralertdata };
-}
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
